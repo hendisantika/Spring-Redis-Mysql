81bbf9d590e6b5a8ff9747a42b758ab278a861d2
Hendi Santika <hendisantika@gmail.com>
2021-06-27 15:00:20 +0700

    #6 - Add save in Country Service

--- src/main/java/com/hendisantika/springredismysql/service/CountryService.java
+++ src/main/java/com/hendisantika/springredismysql/service/CountryService.java
@@ -1,7 +1,9 @@
 package com.hendisantika.springredismysql.service;
 
+import com.hendisantika.springredismysql.entity.Country;
 import com.hendisantika.springredismysql.repository.CountryRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.stereotype.Service;
 
 /**
@@ -19,4 +21,9 @@ public class CountryService {
     private static final String REDIS_CACHE_VALUE = "country";
 
     private CountryRepository countryRepository;
+
+    @CachePut(value = REDIS_CACHE_VALUE, key = "#country.id")
+    public Country save(Country country) {
+        return countryRepository.save(country);
+    }
 }
