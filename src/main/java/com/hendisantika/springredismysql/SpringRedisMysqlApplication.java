df8c7456124666218c0fb75d842590a7a3311e29
Hendi Santika <hendisantika@gmail.com>
2021-06-27 14:48:57 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springredismysql/SpringRedisMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springredismysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRedisMysqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springredismysql/SpringRedisMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springredismysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisMysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
